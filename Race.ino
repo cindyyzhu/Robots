cc48e92be2ae9991aa94207ed3041d4a28f799b2
Cindy <cindyzhu8207@gmail.com>
2026-06-21 09:34:15 -0400

    Add motor control functionality to Race.ino

---
+++ Race.ino
@@ -0,0 +1,49 @@
+const int motor1pin1 = 10;
+const int motor1pin2 = 9;
+const int motor1PWM  = 6;
+
+const int motor2pin1 = 3;
+const int motor2pin2 = 5;
+const int motor2PWM  = 11;
+
+const int touchPin = 13;
+
+void setup() {
+  pinMode(motor1pin1, OUTPUT);
+  pinMode(motor1pin2, OUTPUT);
+  pinMode(motor1PWM,  OUTPUT);
+
+  pinMode(motor2pin1, OUTPUT);
+  pinMode(motor2pin2, OUTPUT);
+  pinMode(motor2PWM,  OUTPUT);
+
+  pinMode(touchPin, INPUT);
+}
+
+void moveForward() {
+  digitalWrite(motor1pin1, HIGH);
+  digitalWrite(motor1pin2, LOW);
+  analogWrite(motor1PWM, 200);
+
+  digitalWrite(motor2pin1, HIGH);
+  digitalWrite(motor2pin2, LOW);
+  analogWrite(motor2PWM, 200);
+}
+
+void stopMotors() {
+  digitalWrite(motor1pin1, LOW);
+  digitalWrite(motor1pin2, LOW);
+  analogWrite(motor1PWM, 0);
+
+  digitalWrite(motor2pin1, LOW);
+  digitalWrite(motor2pin2, LOW);
+  analogWrite(motor2PWM, 0);
+}
+
+void loop() {
+  if (digitalRead(touchPin) == HIGH) {
+    moveForward();
+  } else {
+    stopMotors();
+  }
+}
